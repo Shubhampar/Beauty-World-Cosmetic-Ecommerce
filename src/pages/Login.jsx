81923e6ba2377a802b47d6bfcb9f21aefc3e81e4
Shubhampar <119346050+Shubhampar@users.noreply.github.com>
2023-05-09 01:09:47 +0530

    push

---
+++ src/pages/Login.jsx
@@ -0,0 +1,87 @@
+import React,{useState}from "react";
+import { Link } from "react-router-dom";
+import { Footer, Navbar } from "../components";
+
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Check if user exists with the given email and password
+    const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+    const currentUser = existingUsers.find((user) => user.email === email && user.password === password);
+
+    if (!currentUser) {
+      alert("Incorrect email or password. Please try again.");
+      return;
+    }
+
+    // Sign in successful
+    alert("Sign-in successful!");
+    window.location.href = "/";
+
+  };
+
+  const handleSignUpClick = () => {
+    window.location.href = "./Register.jsx";
+  };
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">Login</h1>
+        <hr />
+        <div class="row my-4 h-100">
+          <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
+            <form onSubmit={handleSubmit}>
+              <div class="my-3">
+                <label for="display-4">Email address</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="floatingInput"
+                  placeholder="name@example.com"
+                  value={email} 
+                  onChange={handleEmailChange}
+                />
+              </div>
+              <div class="my-3">
+                <label for="floatingPassword display-4">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="floatingPassword"
+                  placeholder="Password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+              </div>
+              <div className="my-3">
+                <p>New Here? <Link to="/register" className="text-decoration-underline text-info">Register</Link> </p>
+              </div>
+              <div className="text-center">
+                <button class="my-2 mx-auto btn btn-dark" onClick={handleSignUpClick} type="submit">
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Login;
