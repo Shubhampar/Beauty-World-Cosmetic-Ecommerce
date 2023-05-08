cf83af8df3f73b8645ec7545f75a5aea0311edf4
Shubhampar <119346050+Shubhampar@users.noreply.github.com>
2023-05-09 00:44:50 +0530

    push

--- touted-toad-5222/src/pages/Register.jsx
+++ touted-toad-5222/src/pages/Register.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React,{useState}from 'react'
 import { Footer, Navbar } from "../components";
 import { Link } from 'react-router-dom';
 const Register = () => {
+    const [firstName, setFirstName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isRegistered, setIsRegistered] = useState(false);
+  
+    const handleFirstNameChange = (e) => {
+      setFirstName(e.target.value);
+    };
+    const handleEmailChange = (e) => {
+      setEmail(e.target.value);
+    };
+  
+    const handlePasswordChange = (e) => {
+      setPassword(e.target.value);
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+  
+      // Check if user is already registered with the given email
+      const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+      const isExistingUser = existingUsers.some((user) => user.email === email);
+  
+      if (isExistingUser) {
+        alert("You are already signed up. Please log in.");
+        return;
+      }
+  
+      // Save new user data
+      const newUser = { firstName, email, password };
+      localStorage.setItem("users", JSON.stringify([...existingUsers, newUser]));
+  
+      setIsRegistered(true);
+      window.location.href = "./Login.jsx"
+      
+     
+    };
+  
+    const handleSignInClick = () => {
+      window.location.href = "./Login.jsx"
+    };
     return (
         <>
             <Navbar />
@@ -10,14 +51,19 @@ const Register = () => {
                 <hr />
                 <div class="row my-4 h-100">
                     <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
-                        <form>
+                    {isRegistered ? (
+        {handleSignInClick}
+      ) : (
+                        <form onSubmit={handleSubmit}>
                             <div class="form my-3">
                                 <label for="Name">Full Name</label>
                                 <input
-                                    type="email"
+                                    type="text"
                                     class="form-control"
                                     id="Name"
                                     placeholder="Enter Your Name"
+                                    value={firstName}
+                                    onChange={handleFirstNameChange}
                                 />
                             </div>
                             <div class="form my-3">
@@ -27,6 +73,9 @@ const Register = () => {
                                     class="form-control"
                                     id="Email"
                                     placeholder="name@example.com"
+                                    value={email} 
+                                    onChange={handleEmailChange} 
+                                    required
                                 />
                             </div>
                             <div class="form  my-3">
@@ -36,17 +85,21 @@ const Register = () => {
                                     class="form-control"
                                     id="Password"
                                     placeholder="Password"
+                                    value={password} 
+                                    onChange={handlePasswordChange}
+                                    required
                                 />
                             </div>
                             <div className="my-3">
                                 <p>Already has an account? <Link to="/login" className="text-decoration-underline text-info">Login</Link> </p>
                             </div>
                             <div className="text-center">
-                                <button class="my-2 mx-auto btn btn-dark" type="submit" disabled>
+                                <button class="my-2 mx-auto btn btn-dark" type="submit" onClick={handleSignInClick}>
                                     Register
                                 </button>
                             </div>
                         </form>
+                        )}
                     </div>
                 </div>
             </div>
